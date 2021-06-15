cb5afbd2e6c1015d7dadca86db9b086ad7a8111a
RuaaAlsugheir <alsugheirruaa@gmail.com>
2021-06-15 18:21:32 +0300

    Practice Q1 + 2

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mysql = require("mysql2");
+const connection = require("./db");
 require("dotenv").config();
 const db = require("./db");
 
@@ -30,6 +31,35 @@ app.get("/books", (req, res) => {
       res.status(200);
       res.json(result);
 })
+});
+
+app.put("/books:book_id", (req, res) => {
+    const id = req.params.book_id;
+    const {title, author, is_published, price} = req.body;
+    const query = `UPDATE books SET 
+    title = "${title}",
+    author = "${author}",
+    is_published = "${is_published}",
+    price = "${price}",
+    WHERE id=${id};`;
+    connection.query(query, (error, result) => {
+        if (error) {
+            throw error;
+        }
+
+        res.json(result);
+    })
+});
+
+app.delete("/books:book_id", (req, res) => {
+    const id = req.params.book_id;
+    const query = `DELETE FROM books WHERE id=${id};`;
+    connection.query(query, (error, result) => {
+        if (error) {
+            throw error;
+        }
+        res.json("Deleted successfully");
+    })
 });
 
 app.listen(process.env.PORT, () => {
